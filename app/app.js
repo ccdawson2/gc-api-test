e756e073d7eb2753f026da7ee228a3b603337ae7
ccdawson2 <chrisdwson@gmail.com>
2017-05-26 15:51:42 +1000

    Validation/User Integration
    Validates fields before saving
    User Select populated from users API

--- app/app.js
+++ app/app.js
@@ -7,18 +7,46 @@ new Vue({
 
   // Here we can register any values or collections that hold data for the application
   data: {
-    square: { squareId: '', ownerId: '', xCoord: '', yCoord: '', desc: '', shortDesc: '', image: '' },
-    squares: []
+	errors: [],
+    square: { squareId: '', userId: '', xCoord: '', yCoord: '', desc: '', shortDesc: '', image: '' },
+    squares: [],
+	users: [
+         { text: 'Select a User ..',  value: '0' }
+	],	 
+	selectedUser: '0'	 
   },
 
   created: function() {
-    console.log('created()');
-    this.fetchSquares();
+	
+    this.fetchUsers();
+	this.fetchSquares();
   },
 
   methods: {
 
-    // We dedicate a method to retrieving and setting some data
+    // Create users to populate User Drop Down 
+    fetchUsers: function() {
+
+       this.$http.get('http://users-api-01.appspot.com/users?size=1000').then(response => {
+	      console.log('fetchUsers() ' + response.body.length + ' found');	
+	      console.log(response.body);	
+		  
+		  var i;
+   	      var user = {};
+	   
+          for (i = 0; i < response.body.length; i++) {
+        	 user = {};
+		     user['text']  = response.body[i].fName + " " +
+		                     response.body[i].lName;
+      	     user['value'] = response.body[i].userId;
+             
+			 this.users.push(user);
+          }
+       }, response => { 
+          // error callback 
+       });	
+    },
+
     fetchSquares: function() {
 	
        this.$http.get('http://squares-api-168505.appspot.com/squares/?size=10').then(response => {
@@ -32,12 +60,37 @@ new Vue({
     // Adds an square to the existing squares array
     addSquare: function() {
 		
-  	   console.log("addSquare() " + this.square.squareId);
-	   
+		console.log("selectedUser=" + this.selectedUser);
+		
 	   var body = {};
+
+       if (this.square.squareId == "" || isNaN(this.square.squareId)) {
+		  alert("Square Id must be entered (and Integer)");
+		  return; 
+       }
+       else if (this.square.xCoord == "" || isNaN(this.square.xCoord)) {
+		  alert("X Coord must be entered (and Integer)");
+		  return; 
+       }
+       else if (this.square.yCoord == "" || isNaN(this.square.yCoord)) {
+		  alert("Y Coord must be entered (and Integer)");
+		  return; 
+       }
+       else if (this.square.desc == "") {
+		  alert("Description must be entered");
+		  return; 
+       }
+       else if (this.square.shortDesc == "") {
+		  alert("Short Description must be entered");
+		  return; 
+       }
+       else if (this.selectedUser == '0') {
+		  alert("User must be entered");
+		  return; 
+       };
 	   
 	   body['squareId']  = parseInt(this.square.squareId);
-	   body['ownerId']   = parseInt(this.square.ownerId);
+	   body['userId']    = parseInt(this.selectedUser);
 	   body['xCoord']    = parseInt(this.square.xCoord);
 	   body['yCoord']    = parseInt(this.square.yCoord);
 	   body['desc']      = this.square.desc;
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var jsonfile = require('jsonfile');
 
 var app = express();
 app.use(express.static(__dirname + ''));
@@ -6,6 +7,14 @@ app.use(express.static(__dirname + ''));
 app.get('*', function(req, res) {
 	var options = { root: __dirname };
     res.sendFile('index.html',options);
+});
+
+var file = '..\\dev.txt';
+
+jsonfile.readFile(file, function(err, obj) {
+  if (obj == undefined) {
+	 console.log("dddddd");
+  }	 
 });
 
 var port = "3000";
